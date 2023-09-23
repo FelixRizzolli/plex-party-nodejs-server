f1e74336c584a90bba40945b4f93b895a7cbd328
Felix Rizzolli <felix.rizzolli@outlook.de>
2023-09-23 15:22:02 +0200

    use express, cors and http

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import dotenv from 'dotenv';
+import express, { json } from 'express';
+import http from 'http';
+import cors from 'cors';
+
 import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+
 import { 
   getMovies, 
   getRecentlyAddedMovies,
@@ -17,9 +23,6 @@ import {
 dotenv.config();
 const port: number = Number.parseInt(process.env.PORT) || 4000;
 
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
 const typeDefs = `
   union MediaContent = Movie | TVShow
   scalar Date
@@ -74,19 +77,27 @@ const resolvers = {
   }
 };
 
-// The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
-const server = new ApolloServer({
+interface MyContext {
+  token?: String;
+}
+
+const app = express();
+const httpServer = http.createServer(app);
+const server = new ApolloServer<MyContext>({
   typeDefs,
   resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
 
-// Passing an ApolloServer instance to the `startStandaloneServer` function:
-//  1. creates an Express app
-//  2. installs your ApolloServer instance as middleware
-//  3. prepares your app to handle incoming requests
-const { url } = await startStandaloneServer(server, {
-  listen: { port: port },
-});
+await server.start();
+app.use(
+  '/',
+  cors<cors.CorsRequest>(),
+  json(),
+  expressMiddleware(server, {
+    context: async ({ req }) => ({ token: req.headers.token }),
+  }),
+);
 
-console.log(`🚀  Server ready at: ${url}`);
+await new Promise<void>((resolve) => httpServer.listen({ port: port }, resolve));
+console.log(`🚀 Server ready at http://localhost:${port}/`);
